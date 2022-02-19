c38bcf55630b80214d21c27772010d6694ac0f38
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-02-19 07:35:15 -0300

    feat: create post endpoint for booking

--- src/main/java/com/github/juliherms/pethotel/controller/BookingController.java
+++ src/main/java/com/github/juliherms/pethotel/controller/BookingController.java
@@ -1,10 +1,17 @@
 package com.github.juliherms.pethotel.controller;
 
+import com.github.juliherms.pethotel.dto.BookingDTO;
+import com.github.juliherms.pethotel.model.Booking;
 import com.github.juliherms.pethotel.service.BookingService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -14,5 +21,11 @@ public class BookingController {
     @Autowired
     private BookingService service;
 
-
+    @PostMapping
+    public ResponseEntity<Object> save(@RequestBody @Valid BookingDTO bookingDTO){
+        var booking = new Booking();
+        BeanUtils.copyProperties(bookingDTO,booking);
+        booking.setRegistration(LocalDateTime.now(ZoneId.of("UTC")));
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(booking));
+    }
 }
--- src/main/java/com/github/juliherms/pethotel/service/BookingService.java
+++ src/main/java/com/github/juliherms/pethotel/service/BookingService.java
@@ -1,14 +1,21 @@
 package com.github.juliherms.pethotel.service;
 
+import com.github.juliherms.pethotel.model.Booking;
 import com.github.juliherms.pethotel.repositories.BookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookingService {
 
     @Autowired
     private BookingRepository repo;
+
+    @Transactional
+    public Booking save (Booking booking){
+        return  repo.save(booking);
+    }
 
 
 
