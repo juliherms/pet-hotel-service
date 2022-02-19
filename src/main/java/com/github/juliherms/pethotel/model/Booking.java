24da18765dea201d171572ecac42ba13342594e4
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-02-19 07:00:54 -0300

    feat: introduce booking model

---
+++ src/main/java/com/github/juliherms/pethotel/model/Booking.java
@@ -0,0 +1,29 @@
+package com.github.juliherms.pethotel.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_BOOKING")
+public class Booking implements Serializable {
+    private  static  final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, length = 30)
+    private String petName;
+
+    @Column(nullable = false,length = 70)
+    private String breed;
+
+    @Column(nullable = false,length = 100)
+    private String responsible;
+
+    @Column(nullable = false)
+    private LocalDateTime registration;
+
+}
