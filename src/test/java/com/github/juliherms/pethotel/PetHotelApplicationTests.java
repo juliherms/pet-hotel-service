497155c9fe406e3e985729c2e90cb009f1e009be
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-02-19 06:39:35 -0300

    chore: install spring boot depedencies

---
+++ src/test/java/com/github/juliherms/pethotel/PetHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.juliherms.pethotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
